0bc59fda6d226141120dcada0efb532831ff9935
Daniel do Casal de Paula <daniel.c.paula@hotmail.com>
2022-07-25 16:25:33 -0300

    Rocketnotes-frontend para o deploy

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { useAuth } from '../../hooks/auth'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { api } from '../../services/api'
+import { Container, Profile, Logout } from './styles';
+
+export function Header(){
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img src={avatarUrl} alt="Foto do Usuário" />
+        <div>
+          <span>Bem Vindo,</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={signOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
